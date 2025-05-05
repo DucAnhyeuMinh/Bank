23337ae26087664a7ad06603523db831faa8a1b9
DucanhYeuMinh <11230516@st.neu.edu.vn>
2025-05-05 09:09:39 +0700

    ma

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import streamlit as st
+from Account import User
+from Modules import VisualHandler
+from streamlit_extras.switch_page_button import switch_page
 
-st.write("Hello")
+User.create_user_table() # check if database is created
+
+VisualHandler.initialize_session_state() # create session_state
+
+# main function
+def main():
+    st.balloons()
+    switch_page("Home")
+if __name__ == "__main__":
+    main()
